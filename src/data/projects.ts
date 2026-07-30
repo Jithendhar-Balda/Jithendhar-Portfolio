f281c0c613b366de79acbe5dfa87faa6476d0251
Jithendhar <baldajithendhar@gmail.com>
2026-07-31 03:19:30 +0530

    image path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,13 +2,16 @@
 
 import type { Project } from "../types/project";
 
+import bgscetclg from "../App/assets/images/projects/bgscet.png";
+import portfolio from "../App/assets/images/projects/portfolio.png";
+
 export const projects: Project[] = [
   {
     id: 1,
     title: "BGSCET College Website",
     description:
       "A modern, responsive college website built using React, TypeScript, and CSS3, featuring a scalable component architecture, dynamic department pages, reusable UI components, and an intuitive user experience.",
-    image: "/src/assets/images/projects/bgscet.png",
+    image: bgscetclg,
     github: "https://github.com/naveen-manthri/bgscet",
     technologies: ["React", "TypeScript"],
     category: "Full Stack",
@@ -18,8 +21,8 @@ export const projects: Project[] = [
     title: "Personal Portfolio Website",
     description:
       "A fully responsive portfolio showcasing my projects, skills, and experience. Built using React, TypeScript, and CSS3 with a minimal, modern interface and component-based architecture.",
-    image: "/src/assets/images/projects/portfolio.png",
-    github: "#https://github.com/Jithendhar-Balda/Jithendhar-Portfolio",
+    image: portfolio,
+    github: "https://github.com/Jithendhar-Balda/Jithendhar-Portfolio",
     technologies: ["React", "Typescript", "CSS3"],
     category: "Backend",
   },
